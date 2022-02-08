fff16c8a538a2647edf0cd577f7426fb509fa2d9
girmemesus <99269033+girmemesus@users.noreply.github.com>
2022-02-09 00:27:22 +0800

    Update aichat.py

--- AsunaRobot/modules/aichat.py
+++ AsunaRobot/modules/aichat.py
@@ -365,14 +365,14 @@ async def inuka(client, message):
 
 __help__ = """
 <b> AI Chatbot </b>
-Asuna AI 3.0 IS THE ONLY AI SYSTEM WHICH CAN DETECT & REPLY UPTO 200 LANGUAGES
+Suslizbot AI 0.1 IS THE ONLY AI SYSTEM WHICH CAN DETECT & REPLY UPTO 200 LANGUAGES
 
  - /chatbot [ON/OFF]: Enables and disables AI Chat mode (EXCLUSIVE)
  - /chatbot EN : Enables English only chatbot
  
  
 <b> Chatbot </b>
- - /ask [question]: Ask question from Asuna
+ - /ask [question]: Ask question from Suslizbot
  - /ask [reply to voice note]: Get voice reply
  
 """
