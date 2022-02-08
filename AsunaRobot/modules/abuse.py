265821c8911d6682c4bef6c516e81c7bd0b67c10
girmemesus <99269033+girmemesus@users.noreply.github.com>
2022-02-09 00:25:42 +0800

    Update abuse.py

--- AsunaRobot/modules/abuse.py
+++ AsunaRobot/modules/abuse.py
@@ -5,22 +5,22 @@ from AsunaRobot import dispatcher
 from AsunaRobot.modules.disable import DisableAbleCommandHandler
 
 SFW_STRINGS = (
-    "Jaa na Bsdk, gaand mara jaake.",
-    "Tu paidaishi chutiya hai ki koi course kiya hai? ",
-    "I came to know that some shops were selling your ass for 69$",
+    "Bruh.",
+    "Bahasa tu baik ke? ",
+    "I came to know that some shops were selling your boobs for 69$
     "The world would have been so smooth if your dad had just pulled out",
     "And the truth is, you're a fucking cunt",
     "What else can you do other than spreading your legs?",
     "Jaa na Gandu",
-    "Aand ka na Gaand ka, Gyaan jhaare pure Brahmand ka",
-    "Dhaat teri maa ki ch*t",
-    "Gaand se tatti nikalke jaadugar samajhta hai apne aap ko?",
+    "2000000$ for tell abuse",
+    "You sucks",
+    "Bahasa baik sikit."
     "Bitches be trippin'",
     "Please fuck off bitch, and get a life",
-    "Tu aise nhi maanega - Mat maan, maa chuda",
-    "Tujhse zada sundar teri jali hui gaand hai",
-    "Ye aapki Randikhana nhi hai, kripya yaha se dur rahe",
-    "I'm a good girl, I don't abuse. But you're a bitch.",
+    "hm",
+    "eh",
+    ":/",
+    "I'm a good bot, do not abuse.",
 )
 
 
